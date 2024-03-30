d69508b904f9b6b2089128a924c9a4f0078ef4be
Kim Tuấn Kiên <89715566+kiennkt@users.noreply.github.com>
2024-03-30 23:43:32 +0700

    Update main.cpp

--- Uno_NRF/src/main.cpp
+++ Uno_NRF/src/main.cpp
@@ -9,12 +9,17 @@
 #include <DHT.h>
 #include <Adafruit_Sensor.h>
 
+#define LED_PIN 2
+#define BUZZER_PIN 6
+#define LIGHT_PIN 5
 #define GAS_PIN A0
 #define DHT_PIN A1
 #define DHT_TYPE DHT11
 
-const uint64_t RxAddress = 0xF0F0F0F0E1LL;
+// Address NRF Reciver
+const uint64_t TxAddress = 0xF0F0F0F0E1LL; 
 
+// struct contain all data from sensor
 struct Data {
   float temperature;
   float humidity;
@@ -28,20 +33,11 @@ LiquidCrystal_I2C lcd(0x27,20,4);
 Data data;
 
 
-// NRF reciever
+// NRF transmiter
 void connectNRFModule(){
   radio.begin();
-  radio.openWritingPipe(RxAddress);
+  radio.openWritingPipe(TxAddress);
   radio.stopListening();
-}
-
-// connect DHT
-void connectDHTSensor(){
-  dht11.begin();
-}
-
-void connectGasSensor(){
-  pinMode(GAS_PIN, INPUT);
 }
 
 void connectLCD(){
@@ -58,15 +54,17 @@ void connectLCD(){
 void setup(){  
   Serial.begin(9600);
   connectNRFModule();
-  connectDHTSensor();
-  connectGasSensor();
   connectLCD();
-
+  dht11.begin();
+  pinMode(LIGHT_PIN, INPUT);
+  pinMode(GAS_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
 }
 void loop(){
   data.temperature = dht11.readTemperature();
   data.humidity = dht11.readHumidity();
   data.gas = analogRead(GAS_PIN);
+  int light = digitalRead(LIGHT_PIN);
 
   lcd.setCursor(2, 0);
   lcd.print(data.temperature);
@@ -76,8 +74,19 @@ void loop(){
 
   lcd.setCursor(5, 1);
   lcd.print(data.gas);
-  //Send message to receiver
+  
+  // buzzer
+  if(data.gas >= 350){
+    tone(BUZZER_PIN, 1000);
+  }
+  else noTone(BUZZER_PIN);
 
+  // light sensor
+  if (light == 1){
+    digitalWrite(2, HIGH);
+  }
+  else digitalWrite(2, LOW);
+
+  //Send message to NRF receiver
   radio.write(&data, sizeof(Data));
-
 }
